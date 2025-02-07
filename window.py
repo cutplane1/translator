0336d62c59726a3c5a2d84bf1c383ad5dbc3d9a1
cutplane1 <snakesnake222@mail.ru>
2025-02-07 20:30:23 +0500

    something

--- window.py
+++ window.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'untitled.ui'
+## Form generated from reading UI file 'window.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.8.2
 ##
@@ -30,43 +30,47 @@ class Ui_MainWindow(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
         MainWindow.setSizePolicy(sizePolicy)
+        self.actionGoogle = QAction(MainWindow)
+        self.actionGoogle.setObjectName(u"actionGoogle")
+        self.actionDeepL = QAction(MainWindow)
+        self.actionDeepL.setObjectName(u"actionDeepL")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.horizontalLayoutWidget = QWidget(self.centralwidget)
         self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
         self.horizontalLayoutWidget.setGeometry(QRect(10, 10, 711, 191))
-        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout = QVBoxLayout()
-        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.h_grid = QHBoxLayout(self.horizontalLayoutWidget)
+        self.h_grid.setObjectName(u"h_grid")
+        self.h_grid.setContentsMargins(0, 0, 0, 0)
+        self.textboxes_grid = QVBoxLayout()
+        self.textboxes_grid.setObjectName(u"textboxes_grid")
         self.from_trans = QPlainTextEdit(self.horizontalLayoutWidget)
         self.from_trans.setObjectName(u"from_trans")
 
-        self.verticalLayout.addWidget(self.from_trans)
+        self.textboxes_grid.addWidget(self.from_trans)
 
         self.to_trans = QPlainTextEdit(self.horizontalLayoutWidget)
         self.to_trans.setObjectName(u"to_trans")
 
-        self.verticalLayout.addWidget(self.to_trans)
+        self.textboxes_grid.addWidget(self.to_trans)
 
 
-        self.horizontalLayout.addLayout(self.verticalLayout)
+        self.h_grid.addLayout(self.textboxes_grid)
 
-        self.verticalLayout_3 = QVBoxLayout()
-        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.buttons_grid = QVBoxLayout()
+        self.buttons_grid.setObjectName(u"buttons_grid")
         self.btn_trans = QPushButton(self.horizontalLayoutWidget)
         self.btn_trans.setObjectName(u"btn_trans")
 
-        self.verticalLayout_3.addWidget(self.btn_trans)
+        self.buttons_grid.addWidget(self.btn_trans)
 
         self.btn_flip = QPushButton(self.horizontalLayoutWidget)
         self.btn_flip.setObjectName(u"btn_flip")
 
-        self.verticalLayout_3.addWidget(self.btn_flip)
+        self.buttons_grid.addWidget(self.btn_flip)
 
 
-        self.horizontalLayout.addLayout(self.verticalLayout_3)
+        self.h_grid.addLayout(self.buttons_grid)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
@@ -74,9 +78,14 @@ class Ui_MainWindow(object):
         self.menubar.setGeometry(QRect(0, 0, 739, 22))
         self.menutest = QMenu(self.menubar)
         self.menutest.setObjectName(u"menutest")
+        self.menuProvider = QMenu(self.menutest)
+        self.menuProvider.setObjectName(u"menuProvider")
         MainWindow.setMenuBar(self.menubar)
 
         self.menubar.addAction(self.menutest.menuAction())
+        self.menutest.addAction(self.menuProvider.menuAction())
+        self.menuProvider.addAction(self.actionGoogle)
+        self.menuProvider.addAction(self.actionDeepL)
 
         self.retranslateUi(MainWindow)
 
@@ -85,8 +94,11 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionGoogle.setText(QCoreApplication.translate("MainWindow", u"Google", None))
+        self.actionDeepL.setText(QCoreApplication.translate("MainWindow", u"DeepL", None))
         self.btn_trans.setText(QCoreApplication.translate("MainWindow", u"Translate", None))
         self.btn_flip.setText(QCoreApplication.translate("MainWindow", u"Flip", None))
-        self.menutest.setTitle(QCoreApplication.translate("MainWindow", u"test", None))
+        self.menutest.setTitle(QCoreApplication.translate("MainWindow", u"Settings", None))
+        self.menuProvider.setTitle(QCoreApplication.translate("MainWindow", u"Provider", None))
     # retranslateUi
 
