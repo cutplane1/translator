327730fc7096e3a274b1b1092a9c74633584c261
cutplane1 <snakesnake222@mail.ru>
2025-02-07 19:56:48 +0500

    imprv

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-import sys, keyboard
-from PySide6 import QtWidgets
+import sys
+from PySide6 import QtWidgets, QtCore
 from PySide6.QtGui import QKeySequence, QShortcut
-
 from window import Ui_MainWindow
 
-
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    toggle_visibility = QtCore.Signal()
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
+        
         self.btn_trans.clicked.connect(self.translate)
         self.btn_flip.clicked.connect(self.switch)
-
-    def showEvent(self, event):
-        self.from_trans.setPlainText(self.from_trans.toPlainText()[:-2])
+        
+        self.toggle_visibility.connect(self.flip_window)
+        
+        self.hotkey_thread = HotkeyThread('alt+k', self.toggle_visibility)
+        self.hotkey_thread.start()
 
     def translate(self):
         self.to_trans.setPlainText("DEBUG: " + self.from_trans.toPlainText())
@@ -25,21 +27,37 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.from_trans.setPlainText(p2)
         self.to_trans.setPlainText(p1)
 
+    def flip_window(self):
+        if self.isVisible():
+            self.hide()
+        else:
+            self.show()
+            self.from_trans.setPlainText(self.from_trans.toPlainText()[:-1])
+            self.activateWindow()
 
-app = QtWidgets.QApplication(sys.argv)
-window = MainWindow()
+    def closeEvent(self, event):
+        self.hotkey_thread.stop()
+        super().closeEvent(event)
 
-is_hidden = False
-def flip_window(window):
-    global is_hidden
-    if is_hidden:
-        window.show()
-        is_hidden = False
-    else:
-        window.hide()
-        is_hidden = True
+class HotkeyThread(QtCore.QThread):
+    def __init__(self, hotkey, signal):
+        super().__init__()
+        self.hotkey = hotkey
+        self.signal = signal
+        self.running = True
 
-keyboard.add_hotkey("alt+k", flip_window, args=(window,))
+    def run(self):
+        import keyboard
+        keyboard.add_hotkey(self.hotkey, self.emit_signal)
 
-window.show()
-app.exec()
+    def emit_signal(self):
+        self.signal.emit()
+
+    def stop(self):
+        self.running = False
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
